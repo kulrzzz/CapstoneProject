ef3f872d66a91e63cd5b03a631de9702d8e57a8c
Sukiasmin <aqiljaved110903@gmail.com>
2025-06-01 07:45:37 +0700

    Update

--- app/src/main/java/com/example/capstoneproject/util/Constants.kt
+++ app/src/main/java/com/example/capstoneproject/util/Constants.kt
@@ -1,5 +1,6 @@
 package com.example.capstoneproject.util
 
 object Constants {
-    const val BASE_URL = "http://3.219.80.4:8000/"
+//    const val BASE_URL = "http://3.219.80.4:8000/"
+    const val BASE_URL = "https://apireservin.achmedhibatillah.com/"
 }
